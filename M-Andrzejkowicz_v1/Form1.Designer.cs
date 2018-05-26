6aba7cb55c759238d86c8fd7dbc6c5f634e24c06
kubaadamus <jakubadamus1991@gmail.com>
2018-05-26 11:33:27 +0200

    Pierwszy commit, komunikacja z cmd działa

---
+++ M-Andrzejkowicz_v1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace M_Andrzejkowicz_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.log_textbox = new System.Windows.Forms.TextBox();
+            this.log_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // log_textbox
+            // 
+            this.log_textbox.Location = new System.Drawing.Point(459, 25);
+            this.log_textbox.Multiline = true;
+            this.log_textbox.Name = "log_textbox";
+            this.log_textbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.log_textbox.Size = new System.Drawing.Size(522, 537);
+            this.log_textbox.TabIndex = 0;
+            // 
+            // log_label
+            // 
+            this.log_label.AutoSize = true;
+            this.log_label.Location = new System.Drawing.Point(697, 9);
+            this.log_label.Name = "log_label";
+            this.log_label.Size = new System.Drawing.Size(25, 13);
+            this.log_label.TabIndex = 1;
+            this.log_label.Text = "Log";
+            this.log_label.Click += new System.EventHandler(this.log_label_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 574);
+            this.Controls.Add(this.log_label);
+            this.Controls.Add(this.log_textbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox log_textbox;
+        private System.Windows.Forms.Label log_label;
+    }
+}
+
---
+++ M-Andrzejkowicz_v1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace M_Andrzejkowicz_v1
+{
+    public partial class Form1 : Form
+    {
+        string ipAddress;
+        string[] connections_array;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ipAddress = NetworkGateway();
+            skanuj_button_Click(ipAddress);
+
+
+
+
+        }
+
+
+
+        private void skanuj_button_Click(string ipAddress)
+        {
+            string macAddress = string.Empty;
+            Process Process = new System.Diagnostics.Process();
+            Process.StartInfo.FileName = "netstat";
+            Process.StartInfo.Arguments = "-ano";
+            Process.StartInfo.UseShellExecute = false;
+            Process.StartInfo.RedirectStandardOutput = true;
+            Process.StartInfo.CreateNoWindow = true;
+            Process.Start();
+            string strOutput = Process.StandardOutput.ReadToEnd();
+            while(strOutput.IndexOf("  ")>0)
+            {
+                strOutput = strOutput.Replace("  ", " ");
+            }
+            
+
+            Console.WriteLine("StrOutput" + strOutput);
+            log_textbox.Text = strOutput;
+
+
+            Char delimiter = ' ';
+            String[] substrings = strOutput.Split(delimiter);
+
+            foreach (var substring in substrings)
+            {
+                Console.WriteLine(substring + "//");
+            }
+
+            
+            int matches = Regex.Matches(strOutput, "0.0.0.0").Count;
+            Console.WriteLine("{0} occurrences", matches);
+            Console.WriteLine("Podsłuchuje cie " + matches + " koleszków");
+
+        }
+        static string NetworkGateway()
+        {
+            string ip = null;
+
+            foreach (NetworkInterface f in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (f.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (GatewayIPAddressInformation d in f.GetIPProperties().GatewayAddresses)
+                    {
+                        if (ip == null)
+                            ip = d.Address.ToString();
+                    }
+                }
+            }
+            Console.WriteLine("wypisuje ip");
+            Console.WriteLine(ip);
+            return ip;
+        }
+
+        private void log_label_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
